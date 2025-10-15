1a87ea1ab924af9154bb7d9605a8c0d1d35fd0ef
bijonl <bijon2591@gmail.com>
2025-10-15 16:22:43 -0400

    more styling

--- precious-works-theme-child/blocks/recent-posts/partials/single-post.php
+++ precious-works-theme-child/blocks/recent-posts/partials/single-post.php
@@ -1,14 +1,17 @@
 <?php include(locate_template('components/variables/post-variables.php')); 
-$featured_image = get_the_post_thumbnail($id, 'landscape-thumbnail', array('class' => '')); ?>
+$featured_image = get_the_post_thumbnail($id, 'landscape-thumbnail', array('class' => '')); 
+$date = get_the_date('F j, Y', $id); ?>
 
 <div class="single-post-tile">
-    <div class="single-post-image-wrapper">
-        <?php echo $featured_image ? $featured_image : wp_get_attachment_image($default_blog_image, 'full', false, array('class' => 'w-100 h-auto'))?>
-    </div>
-    <div class="single-post-title-wrapper">
-        <h4 class="mb-0"><?php echo $title ?></h4>
-    </div>
-    <div class="single-post-date-wrapper">
-        <p class="mb-0"><?php echo $publish_date ?></p>
-    </div>
+    <a href=<?php echo $permalink ?> class="color-inherit">
+        <div class="single-post-image-wrapper">
+            <?php echo $featured_image ? $featured_image : wp_get_attachment_image($default_blog_image, 'full', false, array('class' => 'w-100 h-auto'))?>
+        </div>
+        <div class="single-post-title-wrapper">
+            <h4 class="mb-0"><?php echo $title ?></h4>
+        </div>
+        <div class="single-post-date-wrapper">
+            <p class="mb-0"><?php echo $date ?></p>
+        </div>
+    </a>
 </div>
